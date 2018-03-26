9c5c0c949843483a2c40e7b0f71565d80106b102
Krystian Jozwiak <krystian.jozwiak@ericsson.com>
2018-03-26 19:58:16 +0200

    Add new CFDB test case (disabled until test DB will be created)

--- testCFDatabase.py
+++ testCFDatabase.py
@@ -5,8 +5,8 @@ from pathlib import Path
 import cfdatabase
 
 class TestCFDatabase(unittest.TestCase):
-    def testDatabaseOpenSuccess(self):
-        dbPath = "./testdata/test.sqlite"
+    def testDatabaseOpenCreateSuccess(self):
+        dbPath = "./testdata/testtmp.sqlite"
         dbFile = Path(dbPath)
         if dbFile.is_file():
             dbFile.unlink()
@@ -18,7 +18,16 @@ class TestCFDatabase(unittest.TestCase):
         cursor = conn.cursor()
         cursor.execute("SELECT id FROM fix_data LIMIT 1")
         self.assertEqual(len(cursor.fetchall()), 0)
+        conn.close()
         dbFile.unlink()
-
+"""
+    def testDatabaseOpenExistingSuccess(self):
+        dbPath = "./testdata/test.sqlite"
+        conn = cfdatabase.connect(dbPath)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM fix_data LIMIT 1")
+        self.assertGreater(len(cursor.fetchall()), 0)
+        conn.close()
+"""
 if __name__ == '__main__':
     unittest.main()
