b9f2a55d5bac4d977b5f64efb2be1734c2042813
Krystian Jozwiak <krystian.jozwiak@ericsson.com>
2018-04-18 14:10:44 +0200

    Implement cmd diff in CodeChecker wrapper

--- codechecker.py
+++ codechecker.py
@@ -1,6 +1,8 @@
 import config
 
 import subprocess
+import sys
+import json
 
 class CodeChecker():
     def check(self, clean = False):
@@ -14,6 +16,11 @@ class CodeChecker():
         cmd = 'CodeChecker store ' + config.tmpDir + ' -n ' + config.ccRunName + ' --tag ' + tag
         self.runCmd(cmd)
     
+    def diffResolved(self, baseRun, newRun):
+        cmd = 'CodeChecker cmd diff -b ' + baseRun + ' -n ' + newRun + ' --resolved -o json'
+        output = self.runCmd(cmd).decode(sys.stdout.encoding).split('\n', 1)[1]
+        return json.loads(output)
+    
     def runCmd(self, cmd):
         command = '. ' + config.codeCheckerPath + config.codeCheckerRelativeVenv + ' && cd ' + config.codeCheckerPath + config.codeCheckerRelativeBinPath + ' && ./' + cmd
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -23,3 +30,4 @@ class CodeChecker():
 #cc = CodeChecker()
 #cc.check(True)
 #cc.store("0000000")
+#print(cc.diffResolved('example', '/tmp/cctmp'))
